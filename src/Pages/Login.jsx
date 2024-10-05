c3efee690c050628b5b27d302a75bad8d1fc7595
amira-mohamed99 <amira8813417@gmail.com>
2024-10-05 19:10:41 +0300

    Forms

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,31 @@
 import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 function Login() {
   return (
-    <Container>
-      login Page
+    <Container className="mt-4">
+      <Form className="border p-4 rounded">
+        <Form.Group as={Row} className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label column sm="2">
+            Email
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control type="email" placeholder="email@example.com" />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+          <Form.Label column sm="2">
+            Password
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control type="password" placeholder="Password" />
+          </Col>
+        </Form.Group>
+        <Button variant="primary" type="submit">Login</Button>
+      </Form>
     </Container>
   );
 }
