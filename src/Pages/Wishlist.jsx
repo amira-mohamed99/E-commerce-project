bf8b0c157f5c5e1af5c5fd8f8d70cfd21d791270
amira-mohamed99 <amira8813417@gmail.com>
2024-11-01 00:24:10 +0200

    modify

--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,10 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import NewCard from '../components/card';
 import { useShoppingCart } from '../context/ShoppingCartContext';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
 
 const productlist = [
   { id: "1", title: "Portrait1", desc: "Nature View", price: 2000, imgUrl: "images/1654-0112.jpg" },
@@ -27,69 +28,33 @@ const productlist = [
 
 function Wishlist() {
 
-  const { addToWishlist, getItemsQuantity, increaseCartQuantity, decreaseCartQuantity, removeItemFromCart } = useShoppingCart();
+  const { addToWishlist} = useShoppingCart();
 
   if (!addToWishlist || addToWishlist.length === 0) {
     return <Container className='pt-5'><p>Your wishlist is empty.</p></Container>;
   }
   return (
-    <Container className='pt-5 d-flex justify-content-center align-items-center'>
+    <Container className='pt-5'>
       <Row xs={1} md={3} lg={4} className="g-4">
-
         {addToWishlist.map(({ id }) => {
           const item = productlist.find(product => product.id === id);
           if (!item) return null;
-          const { title, price, imgUrl, desc } = item;
-          const quantity = getItemsQuantity(id);
+          return(
+            <Col key={item.id}>
+            <NewCard
+              id={item.id}
+              title={item.title}
+              price={item.price}
+              desc={item.desc}
+              imgUrl={item.imgUrl} />
+          </Col>
 
-          return (
-            <Col key={id} className="d-flex justify-content-center">
-              <Card style={{ width: '18rem', position: 'relative' }} key={id}>
-                <Card.Img variant="top" src={imgUrl} />
-                <div
-                  style={{
-                    position: 'absolute',
-                    top: '89%',
-                    right: '20%',
-                    zIndex: 1,
-                    fontSize: '1.5rem'
-                  }}>
-                  <i
-                    className="bi bi-heart-fill"
-                    style={{
-                      color: 'red',
-                      cursor: 'pointer'
-                    }}
-                  >
-                  </i>
-                </div>
-                <Card.Body>
-                  <Card.Title>{title}</Card.Title>
-                  <Card.Text>{desc}  -  {price}EGP</Card.Text>
-                  <div className='mt-auto'>
-                    {quantity === 0 ? (
-                      <Button variant='primary' className='lg' onClick={() => increaseCartQuantity(id)}>Add to Cart</Button>
-
-                    ) : (
-                      <div className='d-flex align-items-center flex-column ' style={{ gap: "0.5rem" }}>
-                        <div className='d-flex align-items-center justify-content-center'>
-                          <Button size='sm' onClick={() => decreaseCartQuantity(id)}>-</Button>
-                          <span className='fs-3'> {quantity} </span>
-                          <Button size='sm' onClick={() => increaseCartQuantity(id)}>+</Button>
-                        </div>
-                        <Button variant='danger' size='sm' onClick={() => removeItemFromCart(id)}>Remove</Button>
-                      </div>
-                    )}
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-
-
-          )
-        })}
+        
+        );
+      })}
       </Row>
-    </Container>
+
+    </Container >
   );
 }
 
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,59 +7,56 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 const NewCard = ({ id, title, price, desc, imgUrl }) => {
-  const{addWishlistItem} =useShoppingCart();
+  const { addWishlistItem } = useShoppingCart();
 
 
 
   const { getItemsQuantity, increaseCartQuantity, decreaseCartQuantity, removeItemFromCart } = useShoppingCart();
   const quantity = getItemsQuantity(id);
   return (
-    <>
-      {/* {  productlist.map ((item) =>  */}
-      <Container className='d-flex justify-content-center align-items-center'>
-        <Card style={{ width: '18rem', position:'relative' }}>
-          <Card.Img variant="top" src={imgUrl} />
-          <div
+    <Container className='d-flex justify-content-center align-items-center'>
+      <Card style={{ width: '18rem', position: 'relative' }}>
+        <Card.Img variant="top" src={imgUrl} />
+        <div
+          style={{
+            position: 'absolute',
+            top: '89%',
+            right: '20%',
+            zIndex: 1,
+            fontSize: '1.5rem'
+          }}>
+          <i
+            className="bi bi-heart-fill"
             style={{
-              position: 'absolute',
-              top: '89%',
-              right: '20%',
-              zIndex: 1,
-              fontSize: '1.5rem'
-            }}>
-            <i
-              className="bi bi-heart-fill"
-              style={{
-                color: 'red',
-                cursor: 'pointer'
-              }}
-              onClick={() => addWishlistItem(id)}
-            >
-            </i>
-          </div>
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{desc}  -  {price}EGP</Card.Text>
-            <div className='mt-auto'>
-              {quantity === 0 ? (
-                <Button variant='primary' className='lg' onClick={() => increaseCartQuantity(id)}>Add to Cart</Button>
+              color: 'red',
+              cursor: 'pointer'
+            }}
+            onClick={() => addWishlistItem(id)}
+          >
+          </i>
+        </div>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{desc}  -  {price}EGP</Card.Text>
+          <div className='mt-auto'>
+            {quantity === 0 ? (
+              <Button variant='primary' className='lg' onClick={() => increaseCartQuantity(id)}>Add to Cart</Button>
 
-              ) : (
-                <div className='d-flex align-items-center flex-column ' style={{ gap: "0.5rem" }}>
-                  <div className='d-flex align-items-center justify-content-center'>
-                    <Button size='sm' onClick={() => decreaseCartQuantity(id)}>-</Button>
-                    <span className='fs-3'> {quantity} </span>
-                    <Button size='sm' onClick={() => increaseCartQuantity(id)}>+</Button>
-                  </div>
-                  <Button variant='danger' size='sm' onClick={() => removeItemFromCart(id)}>Remove</Button>
+            ) : (
+              <div className='d-flex align-items-center flex-column ' style={{ gap: "0.5rem" }}>
+                <div className='d-flex align-items-center justify-content-center'>
+                  <Button size='sm' onClick={() => decreaseCartQuantity(id)}>-</Button>
+                  <span className='fs-3'> {quantity} </span>
+                  <Button size='sm' onClick={() => increaseCartQuantity(id)}>+</Button>
                 </div>
-              )}
-            </div>
-          </Card.Body>
-        </Card>
-      </Container>
-      {/* )} */}
-    </>
+                <Button variant='danger' size='sm' onClick={() => removeItemFromCart(id)}>Remove</Button>
+              </div>
+            )}
+          </div>
+        </Card.Body>
+      </Card>
+    </Container>
+
   );
 }
 export default NewCard;
